b3cddbdfedc7adf3851793a8fac19069d4bfa014
John Learn <jflearn@logicbrush.com>
2026-06-12 14:05:28 -0400

    footer

--- public/themes/default/_page.footer.html.php
+++ public/themes/default/_page.footer.html.php
@@ -1,3 +1,4 @@
 <footer id="page-footer" class="container" style="text-align:center">
+    <p>Made with 😣 by <a href="https://www.logicbrush.com" target="_blank">Logicbrush Studios, ltd.</a></p>
     <p>✉️ <a href="mailto:support@logicbrush.com">support@logicbrush.com</a></p>
 </footer>
